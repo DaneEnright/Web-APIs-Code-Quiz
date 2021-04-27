ec9ae8ec4e50f0ae204619cc33b4c25e414ace4f
Dane Enright <enrightco@comcast.net>
2021-04-27 09:50:20 -0600

    added to the function startQuiz

--- script.js
+++ script.js
@@ -49,13 +49,35 @@ var question3 = "The condtion of an if/else statement is enclosed within:";
 var question4= "String values must be enclosed within _________ when being assigned variables ";
 var question5= "A very useful tool used during development and debugging for printing content to debugger is ";
 
-
+for question1("quest1") {
+    question1.textcontent = ;
+    commonData.textcontent = [i];
+}
+for question2("quest2") {
+    question2.textcontent = ;
+    arrayJavaScript.textcontent = [i];
+}
+for question3("quest3") {
+    question3.textcontent = ;
+    ifElseStatement.textcontent = [i];
+}
+for question4("quest4") {
+    question4.textcontent = ;
+    stringValues.textcontent = [i];
+}
+for question5("quest5") {
+    question5.textcontent = ;
+    deBugger.textcontent = [i];
+}
 
 
 }
 
 
+function addHighScores() {
 
+
+}
 
 
 
@@ -81,9 +103,4 @@ var question5= "A very useful tool used during development and debugging for pri
 
 
 
-// var carousel = document.querySelector(".carouselbox");
-// // TODO: Which element is the following line of code selecting?
-// var next = carousel.querySelector(".next"); // This line is selecting class=next as the element
-// var prev = carousel.querySelector(".prev"); // This line is selecting class=prev as the element
-// var index = 0; //Set invitial value in the array to the first element
-// var currentImage;
+
